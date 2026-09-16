4d4ace36b3292b3c1c71431d89965a8d300b0cf5
Xenon3112 <jayjun2007@gmail.com>
2026-09-16 10:42:33 +0700

    Complete assignment 5.1

---
+++ lab1/ProgramCalculate.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class ProgramCalculate {
+    public static void main(String[] args) {
+        String strNum1 ;
+        String strNum2 ;
+
+        strNum1 = JOptionPane.showInputDialog(null,
+                "Please input the first number: ", "Input the first number",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        strNum2 = JOptionPane.showInputDialog(null,
+                "Please input the second number: ", "Input the second number",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("difference: " + difference);
+        System.out.println("product: " + product);
+        System.out.println("quotient: " + quotient);
+    }
+}
